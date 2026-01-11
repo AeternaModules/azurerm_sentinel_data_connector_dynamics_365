05262c7e80719776011c1e944351b135f2cb9302
Daniel Allen <dan@aeternagroup.net>
2026-01-11 18:05:04 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "sentinel_data_connector_dynamics_365s" {
+  description = "All sentinel_data_connector_dynamics_365 resources"
+  value       = azurerm_sentinel_data_connector_dynamics_365.sentinel_data_connector_dynamics_365s
+}
+
