2475ac7707191bb6160cb17ca84777adfb320ffb
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:14:55 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,16 @@ output "sentinel_data_connector_dynamics_365s" {
   description = "All sentinel_data_connector_dynamics_365 resources"
   value       = azurerm_sentinel_data_connector_dynamics_365.sentinel_data_connector_dynamics_365s
 }
+output "sentinel_data_connector_dynamics_365s_log_analytics_workspace_id" {
+  description = "List of log_analytics_workspace_id values across all sentinel_data_connector_dynamics_365s"
+  value       = [for k, v in azurerm_sentinel_data_connector_dynamics_365.sentinel_data_connector_dynamics_365s : v.log_analytics_workspace_id]
+}
+output "sentinel_data_connector_dynamics_365s_name" {
+  description = "List of name values across all sentinel_data_connector_dynamics_365s"
+  value       = [for k, v in azurerm_sentinel_data_connector_dynamics_365.sentinel_data_connector_dynamics_365s : v.name]
+}
+output "sentinel_data_connector_dynamics_365s_tenant_id" {
+  description = "List of tenant_id values across all sentinel_data_connector_dynamics_365s"
+  value       = [for k, v in azurerm_sentinel_data_connector_dynamics_365.sentinel_data_connector_dynamics_365s : v.tenant_id]
+}
 
