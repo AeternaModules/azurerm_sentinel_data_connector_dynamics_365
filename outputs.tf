fdfc3028827becfc5aa0e383e403b9f1fcba88ee
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:13 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "sentinel_data_connector_dynamics_365s" {
-  description = "All sentinel_data_connector_dynamics_365 resources"
-  value       = azurerm_sentinel_data_connector_dynamics_365.sentinel_data_connector_dynamics_365s
-}
 output "sentinel_data_connector_dynamics_365s_log_analytics_workspace_id" {
-  description = "List of log_analytics_workspace_id values across all sentinel_data_connector_dynamics_365s"
-  value       = [for k, v in azurerm_sentinel_data_connector_dynamics_365.sentinel_data_connector_dynamics_365s : v.log_analytics_workspace_id]
+  description = "Map of log_analytics_workspace_id values across all sentinel_data_connector_dynamics_365s, keyed the same as var.sentinel_data_connector_dynamics_365s"
+  value       = { for k, v in azurerm_sentinel_data_connector_dynamics_365.sentinel_data_connector_dynamics_365s : k => v.log_analytics_workspace_id }
 }
 output "sentinel_data_connector_dynamics_365s_name" {
-  description = "List of name values across all sentinel_data_connector_dynamics_365s"
-  value       = [for k, v in azurerm_sentinel_data_connector_dynamics_365.sentinel_data_connector_dynamics_365s : v.name]
+  description = "Map of name values across all sentinel_data_connector_dynamics_365s, keyed the same as var.sentinel_data_connector_dynamics_365s"
+  value       = { for k, v in azurerm_sentinel_data_connector_dynamics_365.sentinel_data_connector_dynamics_365s : k => v.name }
 }
 output "sentinel_data_connector_dynamics_365s_tenant_id" {
-  description = "List of tenant_id values across all sentinel_data_connector_dynamics_365s"
-  value       = [for k, v in azurerm_sentinel_data_connector_dynamics_365.sentinel_data_connector_dynamics_365s : v.tenant_id]
+  description = "Map of tenant_id values across all sentinel_data_connector_dynamics_365s, keyed the same as var.sentinel_data_connector_dynamics_365s"
+  value       = { for k, v in azurerm_sentinel_data_connector_dynamics_365.sentinel_data_connector_dynamics_365s : k => v.tenant_id }
 }
 
