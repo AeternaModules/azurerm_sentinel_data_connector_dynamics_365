202d7b989ec66742ee5cd47776de6b600aead374
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:50:42 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
 output "sentinel_data_connector_dynamics_365s_id" {
   description = "Map of id values across all sentinel_data_connector_dynamics_365s, keyed the same as var.sentinel_data_connector_dynamics_365s"
-  value       = { for k, v in azurerm_sentinel_data_connector_dynamics_365.sentinel_data_connector_dynamics_365s : k => v.id }
+  value       = { for k, v in azurerm_sentinel_data_connector_dynamics_365.sentinel_data_connector_dynamics_365s : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "sentinel_data_connector_dynamics_365s_log_analytics_workspace_id" {
   description = "Map of log_analytics_workspace_id values across all sentinel_data_connector_dynamics_365s, keyed the same as var.sentinel_data_connector_dynamics_365s"
-  value       = { for k, v in azurerm_sentinel_data_connector_dynamics_365.sentinel_data_connector_dynamics_365s : k => v.log_analytics_workspace_id }
+  value       = { for k, v in azurerm_sentinel_data_connector_dynamics_365.sentinel_data_connector_dynamics_365s : k => v.log_analytics_workspace_id if v.log_analytics_workspace_id != null && length(v.log_analytics_workspace_id) > 0 }
 }
 output "sentinel_data_connector_dynamics_365s_name" {
   description = "Map of name values across all sentinel_data_connector_dynamics_365s, keyed the same as var.sentinel_data_connector_dynamics_365s"
-  value       = { for k, v in azurerm_sentinel_data_connector_dynamics_365.sentinel_data_connector_dynamics_365s : k => v.name }
+  value       = { for k, v in azurerm_sentinel_data_connector_dynamics_365.sentinel_data_connector_dynamics_365s : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "sentinel_data_connector_dynamics_365s_tenant_id" {
   description = "Map of tenant_id values across all sentinel_data_connector_dynamics_365s, keyed the same as var.sentinel_data_connector_dynamics_365s"
-  value       = { for k, v in azurerm_sentinel_data_connector_dynamics_365.sentinel_data_connector_dynamics_365s : k => v.tenant_id }
+  value       = { for k, v in azurerm_sentinel_data_connector_dynamics_365.sentinel_data_connector_dynamics_365s : k => v.tenant_id if v.tenant_id != null && length(v.tenant_id) > 0 }
 }
 
